309a16e433bd602f79a478192cb32fec069f15c4
Graham Gardiner <gardinergraham@yahoo.co.uk>
2026-01-23 11:28:37 +0000

    small letters tiktok links

--- app/TikTok/page.tsx
+++ app/TikTok/page.tsx
@@ -2,7 +2,7 @@ import TikTokStore from "./tiktok";
 
 export const metadata = {
   title: "Download PartyShare",
-  description: "Get PartyShare on the App Store and Google Play",
+  description: "Get Party Share on the App Store and Google Play",
 };
 
 export default function Page() {
