1f90c3bab71c1ee442f8b4026cc3de50e37d4a24
Graham Gardiner <gardinergraham@yahoo.co.uk>
2026-02-27 13:51:21 +0000

    Update icon and web page 2 updated marketing web

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,14 +2,11 @@ import "./globals.css";
 import { ReactNode } from "react";
 
 export const metadata = {
+  title: "PartyShare Guest Portal",
+  description: "Access events and guestbooks easily with PartyShare",
   icons: {
     icon: "/icon.png", // or "/icon.png"
   },
-};
-
-export const metadata = {
-  title: "PartyShare Guest Portal",
-  description: "Access events and guestbooks easily with PartyShare",
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
